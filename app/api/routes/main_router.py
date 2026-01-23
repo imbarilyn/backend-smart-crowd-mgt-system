82800786a386b14bf682338185821bd3d78a1ccd
dev-lyn <linahmuhonjaimbari@gmail.com>
2026-01-23 23:07:01 +0300

    add a for loop for the routers

--- app/api/routes/main_router.py
+++ app/api/routes/main_router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from app.api.endpoints.event.event import router as event_router
+from app.api.endpoints.payments.payments import router as payments_router
 
 
 router = APIRouter()
-router.include_router(event_router)
+all_routes = [event_router, payments_router]
+for i in all_routes:
+    router.include_router(i)
